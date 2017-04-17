5a1c776dc9d9ae071f96b367b74353cb4a5c64fe
Tars.Liu <liutao@seuic.com>
2017-04-17 11:08:57 +0800

    hello world

--- src/main/java/com/tarsliu/TrainingApplication.java
+++ src/main/java/com/tarsliu/TrainingApplication.java
@@ -2,9 +2,17 @@ package com.tarsliu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TrainingApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
